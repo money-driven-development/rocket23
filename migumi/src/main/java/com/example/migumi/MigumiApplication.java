2ddb16e06746cba11f684e03f7fcd622b02c61c1
pj991207 <pj991207@gmail.com>
2023-05-05 15:18:54 +0900

    chore : spring boot 생성, issue template 설정

---
+++ migumi/src/main/java/com/example/migumi/MigumiApplication.java
@@ -0,0 +1,13 @@
+package com.example.migumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigumiApplication.class, args);
+    }
+
+}
---
+++ migumi/src/test/java/com/example/migumi/MigumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
