da69f667ef825ab25d8df285348711b5619c78d3
pj991207 <pj991207@gmail.com>
2023-05-23 15:32:57 +0900

    [IC-10] chore: 사용하지 않는 import 구문 삭제

--- src/main/java/com/initcloud/rocket23/file/service/FileService.java
+++ src/main/java/com/initcloud/rocket23/file/service/FileService.java
@@ -1,10 +1,6 @@
 package com.initcloud.rocket23.file.service;
 
-import com.initcloud.rocket23.file.dto.FileDto;
-
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Map;
 
 public interface FileService {
 	void init();
