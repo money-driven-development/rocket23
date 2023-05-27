de70a13b130fd7365578f036abccccdc7099cd49
Floodnut <gsniper777@gmail.com>
2023-05-27 20:06:15 +0900

    [IC-18] feat: ContainerAPI 인터페이스 작성 (#9)

---
+++ src/main/java/com/initcloud/rocket23/container/middleware/ContainerApi.java
@@ -0,0 +1,12 @@
+package com.initcloud.rocket23.container.middleware;
+
+public interface ContainerApi {
+
+	public void create();
+
+	public void execute();
+
+	public void get();
+
+	public void terminate();
+}
