e087860966d4962f00e43492fd91c6dc5beb475b
Floodnut <gsniper777@gmail.com>
2023-05-27 20:07:29 +0900

    [IC-18] feat: 컨테이너 실행 기본 메서드 작성 (#9)

--- src/main/java/com/initcloud/rocket23/container/service/DockerService.java
+++ src/main/java/com/initcloud/rocket23/container/service/DockerService.java
@@ -1,39 +1,46 @@
 package com.initcloud.rocket23.container.service;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DockerService implements ContainerService {
 
 	/**
-	 * 도커 컨테이너를 실행.
+	 * 도커 컨테이너를 실행. 스캔 명령이 함께 동작.
+	 * @param count 만큼 컨테이너 실행.
+	 * count 가 null 일 경우 단일(1개) 실행.
 	 */
 	@Override
-	public void executeContainer() {
+	public void executeContainer(Integer count) {
 
 	}
 
 	/**
-	 * 도커 컨테이너를 대기상태로 실행.
+	 * 도커 컨테이너를 실행 후 대기.
+	 * @param count 만큼 컨테이너 실행
+	 * count 가 null 일 경우 단일(1개) 실행.
 	 */
 	@Override
-	public void executeContainerForStandBy() {
+	public void executeContainerForStandBy(@Nullable Integer count) {
 
 	}
 
 	/**
 	 * 도커 컨테이너를 종료.
+	 * @param containerId 를 대상으로 함.
 	 */
 	@Override
-	public void terminateContainer() {
+	public void terminateContainer(String containerId) {
 
 	}
 
 	/**
 	 * 도커 컨테이너 정보를 조회.
+	 * @param containerId 를 대상으로 함.
 	 */
 	@Override
-	public void getContainer() {
+	public void getContainer(String containerId) {
 
 	}
 
@@ -47,9 +54,10 @@ public class DockerService implements ContainerService {
 
 	/**
 	 * 도커 컨테이너을 일시정지.
+	 * @param containerId 를 대상으로 함.
 	 */
 	@Override
-	public void pauseContainer() {
+	public void pauseContainer(String containerId) {
 
 	}
 }
