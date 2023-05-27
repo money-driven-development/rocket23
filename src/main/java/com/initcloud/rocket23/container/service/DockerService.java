1044e20d058dcd8508f943ae59744b677f171547
Floodnut <gsniper777@gmail.com>
2023-05-27 19:05:42 +0900

    [IC-18] feat: 도커 컨테이너 서비스에 대한 주석 개략 작성 (#9)

--- src/main/java/com/initcloud/rocket23/container/service/DockerService.java
+++ src/main/java/com/initcloud/rocket23/container/service/DockerService.java
@@ -5,31 +5,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class DockerService implements ContainerService {
 
+	/**
+	 * 도커 컨테이너를 실행.
+	 */
 	@Override
 	public void executeContainer() {
 
 	}
 
+	/**
+	 * 도커 컨테이너를 대기상태로 실행.
+	 */
 	@Override
 	public void executeContainerForStandBy() {
 
 	}
 
+	/**
+	 * 도커 컨테이너를 종료.
+	 */
 	@Override
 	public void terminateContainer() {
 
 	}
 
+	/**
+	 * 도커 컨테이너 정보를 조회.
+	 */
 	@Override
 	public void getContainer() {
 
 	}
 
+	/**
+	 * 도커 컨테이너 목록을 조회.
+	 */
 	@Override
 	public void getContainerList() {
 
 	}
 
+	/**
+	 * 도커 컨테이너을 일시정지.
+	 */
 	@Override
 	public void pauseContainer() {
 
