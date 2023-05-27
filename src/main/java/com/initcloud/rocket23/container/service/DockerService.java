746785b4e197817009e533cac1c33fc32d0cabf7
Floodnut <gsniper777@gmail.com>
2023-05-27 19:02:38 +0900

    [IC-18] feat: 컨테이너 및 도커 서비스 인터페이스 추가 (#9)

---
+++ src/main/java/com/initcloud/rocket23/container/service/DockerService.java
@@ -0,0 +1,37 @@
+package com.initcloud.rocket23.container.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DockerService implements ContainerService {
+
+	@Override
+	public void executeContainer() {
+
+	}
+
+	@Override
+	public void executeContainerForStandBy() {
+
+	}
+
+	@Override
+	public void terminateContainer() {
+
+	}
+
+	@Override
+	public void getContainer() {
+
+	}
+
+	@Override
+	public void getContainerList() {
+
+	}
+
+	@Override
+	public void pauseContainer() {
+
+	}
+}
