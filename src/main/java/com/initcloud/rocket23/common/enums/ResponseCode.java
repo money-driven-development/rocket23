b0590ff099cd7fcd7f3d298ea24825f6b7aa3c98
Floodnut <gsniper777@gmail.com>
2023-05-27 19:16:41 +0900

    [IC-18] feat: 도커 컨테이너 기본 예외 추가 (#9)

--- src/main/java/com/initcloud/rocket23/common/enums/ResponseCode.java
+++ src/main/java/com/initcloud/rocket23/common/enums/ResponseCode.java
@@ -10,17 +10,29 @@ import lombok.Getter;
 public enum ResponseCode {
 
 	/* Invalid Request */
-	INVALID_REQUEST(4001, HttpStatus.BAD_REQUEST, "Invalid Request."),
-	DATA_MISSING(4002, HttpStatus.INTERNAL_SERVER_ERROR, "BAD_REQUEST"),
+	/**
+	 * 4000 ~ 4199: Common, Auth Exception
+	 */
+	INVALID_REQUEST(4001, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+	/**
+	 * 42xx: File Exception
+	 */
+
+	/**
+	 * 43xx: Container Exception
+	 * @Todo - 추후, 컨테이너 관련 예외는 4xx와 5xx 에러를 명확하게 구분해야 함.
+	 */
+	DOCKER_CANNOT_RUN_CONTAINER(4301, HttpStatus.BAD_REQUEST, "컨테이너를 실행할 수 없습니다."),
+	DOCKER_CANNOT_GET_CONTAINER(4302, HttpStatus.BAD_REQUEST, "도커 컨테이너를 조회할 수 없습니다."),
+	DOCKER_CANNOT_GET_CONTAINER_LIST(4303, HttpStatus.BAD_REQUEST, "도커 컨테이너 목록을 조회할 수 없습니다."),
+	DOCKER_CANNOT_TERMINATE_CONTAINER(4304, HttpStatus.BAD_REQUEST, "도커 컨테이너를 종료할 수 없습니다."),
+
 	/* Server Error. */
-	SERVER_BUSY(5001, HttpStatus.INTERNAL_SERVER_ERROR, "Server busy."),
-	SCAN_ERROR(5002, HttpStatus.INTERNAL_SERVER_ERROR, "Scan Error."),
-	SERVER_ERROR(5100, HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error."),
-	/*File Uplaod Error. */
-	SERVER_CREATED_DIR_ERROR(5201, HttpStatus.INTERNAL_SERVER_ERROR, "디렉토리를 생성할 수 없습니다."),
-	SERVER_STORE_ERROR(5202, HttpStatus.INTERNAL_SERVER_ERROR, "서버 저장 오류입니다."),
-	FILE_EMPTY(5203, HttpStatus.INTERNAL_SERVER_ERROR, "빈 파일입니다."),
-	AWS_FILE_UPLOAD_ERROR(5204, HttpStatus.INTERNAL_SERVER_ERROR, "AWS 파일 업로드 오류입니다.");
+	SERVER_BUSY(5001, HttpStatus.INTERNAL_SERVER_ERROR, "서버가 바쁩니다. 잠시 후 다시 시도해주세요."),
+
+	/* 미확인 에러 */
+	SERVER_ERROR(5100, HttpStatus.INTERNAL_SERVER_ERROR, "오류가 발생했습니다.");
 
 	private final int code;
 	private final HttpStatus httpStatus;
