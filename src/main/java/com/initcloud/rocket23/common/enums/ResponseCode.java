35038f33245fa337e4b4a1d2a5442daa9816b9cf
pj991207 <pj991207@gmail.com>
2023-05-31 20:48:47 +0900

    [IC-10] chore : 코드리뷰 반영, 예외처리 분리 (#5)

---
+++ src/main/java/com/initcloud/rocket23/common/enums/ResponseCode.java
@@ -0,0 +1,30 @@
+package com.initcloud.rocket23.common.enums;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ResponseCode {
+
+	/* Invalid Request */
+	INVALID_REQUEST(4001, HttpStatus.BAD_REQUEST, "Invalid Request."),
+
+	DATA_MISSING(4002, HttpStatus.INTERNAL_SERVER_ERROR, "BAD_REQUEST"),
+	/* Server Error. */
+	SERVER_BUSY(5001, HttpStatus.INTERNAL_SERVER_ERROR, "Server busy."),
+	SCAN_ERROR(5002, HttpStatus.INTERNAL_SERVER_ERROR, "Scan Error."),
+
+	SERVER_ERROR(5100, HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error."),
+	/*File Uplaod Error. */
+	SERVER_CREATED_DIR_ERROR(5101, HttpStatus.INTERNAL_SERVER_ERROR, "Could not create upload directory"),
+	SERVER_STORE_ERROR(5102, HttpStatus.INTERNAL_SERVER_ERROR, "Could not store the file"),
+	FILE_EMPTY(5103, HttpStatus.INTERNAL_SERVER_ERROR, "빈 파일입니다."),
+	AWS_S3_ERROR(5104, HttpStatus.INTERNAL_SERVER_ERROR, "AWS 파일 업로드 오류입니다.");
+
+	private final int code;
+	private final HttpStatus httpStatus;
+	private final String message;
+}
