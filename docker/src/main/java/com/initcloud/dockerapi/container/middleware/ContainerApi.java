19704262e3dd5515c43435acaa2a861f10761895
Floodnut <gsniper777@gmail.com>
2023-06-10 00:21:18 +0900

    [IC-18] feat: 컨테이너 조회, 수정, 종료, 실행, 이미지 풀링에 대한 구현 추가 (#9)

--- docker/src/main/java/com/initcloud/dockerapi/container/middleware/ContainerApi.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/middleware/ContainerApi.java
@@ -2,13 +2,18 @@ package com.initcloud.dockerapi.container.middleware;
 
 public interface ContainerApi {
 
+	/* 생성 */
 	public void create();
 
+	/* 실행 */
 	public void execute();
 
+	/* 목록 조회 */
 	public void get();
 
-	public void terminate();
+	/* 세부 조회 */
+	public void inspect(String containerId);
 
-	void pull() throws InterruptedException;
+	/* 종료 */
+	public void terminate(String containerId);
 }
