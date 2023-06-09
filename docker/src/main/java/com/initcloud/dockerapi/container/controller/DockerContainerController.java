60070a7c1c5175f82d2a927ff41574bc509a1466
Floodnut <gsniper777@gmail.com>
2023-06-10 01:51:53 +0900

    [IC-18] feat: 컨테이너 목록 조회, 세부 조회 API를 구분 (#9)

--- docker/src/main/java/com/initcloud/dockerapi/container/controller/DockerContainerController.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/controller/DockerContainerController.java
@@ -2,12 +2,13 @@ package com.initcloud.dockerapi.container.controller;
 
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.initcloud.dockerapi.common.dto.ResponseDto;
+import com.initcloud.dockerapi.container.dto.DockerDto;
 import com.initcloud.dockerapi.container.service.DockerService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,14 +21,19 @@ public class DockerContainerController {
 	private final DockerService dockerService;
 
 	@GetMapping
-	public ResponseDto containerDetails(@Nullable @RequestParam String containerId) {
+	public ResponseDto containerList() {
 
-		if (containerId == null)
-			dockerService.getContainerList();
-		else
-			dockerService.getContainer(containerId);
+		dockerService.getContainerList();
 
 		return new ResponseDto<>(null);
+	}
+
+	@GetMapping("/{containerId}")
+	public ResponseDto<DockerDto.Container> containerDetails(@Nullable @PathVariable String containerId) {
+
+		DockerDto.Container response = dockerService.getContainerDetails(containerId);
+
+		return new ResponseDto<>(response);
 	}
 
 	@PostMapping
