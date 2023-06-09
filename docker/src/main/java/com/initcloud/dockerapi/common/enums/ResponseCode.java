32bf5007a489e38b4a86a6de5cac6cece132d879
Floodnut <gsniper777@gmail.com>
2023-06-09 21:28:26 +0900

    [IC-18] feat: 도커 클라이언트의 오류를 식별하도록 타입 추가 (#9)

--- docker/src/main/java/com/initcloud/dockerapi/common/enums/ResponseCode.java
+++ docker/src/main/java/com/initcloud/dockerapi/common/enums/ResponseCode.java
@@ -27,12 +27,19 @@ public enum ResponseCode {
 	DOCKER_CANNOT_GET_CONTAINER(4302, HttpStatus.BAD_REQUEST, "도커 컨테이너를 조회할 수 없습니다."),
 	DOCKER_CANNOT_GET_CONTAINER_LIST(4303, HttpStatus.BAD_REQUEST, "도커 컨테이너 목록을 조회할 수 없습니다."),
 	DOCKER_CANNOT_TERMINATE_CONTAINER(4304, HttpStatus.BAD_REQUEST, "도커 컨테이너를 종료할 수 없습니다."),
+	DOCKER_IMAGE_NOT_FOUND(4305, HttpStatus.BAD_REQUEST, "도커 이미지를 찾을 수 없습니다."),
 
 	/* Server Error. */
 	SERVER_BUSY(5001, HttpStatus.INTERNAL_SERVER_ERROR, "서버가 바쁩니다. 잠시 후 다시 시도해주세요."),
 
 	/* 미확인 에러 */
-	SERVER_ERROR(5100, HttpStatus.INTERNAL_SERVER_ERROR, "오류가 발생했습니다.");
+	SERVER_ERROR(5100, HttpStatus.INTERNAL_SERVER_ERROR, "오류가 발생했습니다."),
+
+	/**
+	 * 53xx: Container Exception
+	 * @Todo - 추후, 컨테이너 관련 예외는 4xx와 5xx 에러를 명확하게 구분해야 함.
+	 */
+	NULL_DOCKER_CLIENT(4305, HttpStatus.INTERNAL_SERVER_ERROR, "실행 중인 도커 데몬 정보가 없습니다.");
 
 	private final int code;
 	private final HttpStatus httpStatus;
