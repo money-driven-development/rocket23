dc65c0f088b0ea470dbbb398c26ddd68eb4dbde9
Floodnut <gsniper777@gmail.com>
2023-06-10 00:21:18 +0900

    [IC-18] feat: 컨테이너 조회, 수정, 종료, 실행, 이미지 풀링에 대한 구현 추가 (#9)

--- docker/src/main/java/com/initcloud/dockerapi/container/middleware/DockerContainerApi.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/middleware/DockerContainerApi.java
@@ -4,13 +4,13 @@ import java.security.SecureRandom;
 
 import org.springframework.stereotype.Service;
 
-import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 
 import com.github.dockerjava.api.exception.NotFoundException;
 import com.initcloud.dockerapi.common.enums.ResponseCode;
 import com.initcloud.dockerapi.common.exception.ApiException;
 import com.initcloud.dockerapi.container.client.DockerContainerClient;
+import com.initcloud.dockerapi.container.enums.DockerImages;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -22,61 +22,68 @@ import lombok.extern.slf4j.Slf4j;
 public class DockerContainerApi implements ContainerApi {
 
 	private static String CONTAINER_NAME_PREFIX = "rocket23_";
-	private static String IMAGE_ALPINE = "alpine";
 	private final DockerContainerClient dockerContainerClient = new DockerContainerClient();
 
 	@Override
 	public void create() {
-		create(IMAGE_ALPINE);
+		this.create(DockerImages.ALPINE_LATEST);
 	}
 
-	public void create(String image) {
+	public void create(DockerImages image) {
 		try {
 			String containerName = CONTAINER_NAME_PREFIX + new SecureRandom().nextInt();
-			pull();
+			this.pull(image);
+
 			CreateContainerResponse container = dockerContainerClient.getDockerClient()
-				.createContainerCmd(IMAGE_ALPINE)
+				.createContainerCmd(DockerImages.getFullImageName(image))
 				.withCmd("env")
 				.withName(containerName)
 				.exec();
 
 			log.info("Created container {}", container.toString());
 		} catch (NullPointerException e) {
-			throw new ApiException(e, ResponseCode.DOCKER_IMAGE_NOT_FOUND);
+			throw new ApiException(e, ResponseCode.NULL_DOCKER_CLIENT);
 		} catch (NotFoundException e) {
 			log.info("Not Found Image {}", image);
 			throw new ApiException(e, ResponseCode.DOCKER_IMAGE_NOT_FOUND);
+		} catch (InterruptedException e) {
+			log.warn("Couldn't pull Image {}", image);
+			throw new ApiException(e, ResponseCode.DOCKER_CANNOT_PULL_IMAGE);
 		}
 	}
 
 	@Override
 	public void execute() {
-		DockerClient dockerClient = dockerContainerClient.getDockerClient();
-		dockerClient.pingCmd().exec();
+		dockerContainerClient.getDockerClient()
+			.pingCmd()
+			.exec();
+	}
+
+	@Override
+	public void inspect(String containerId) {
+		dockerContainerClient.getDockerClient()
+			.inspectContainerCmd(containerId)
+			.exec();
 	}
 
 	@Override
 	public void get() {
-
+		dockerContainerClient.getDockerClient()
+			.listContainersCmd()
+			.exec();
 	}
 
 	@Override
-	public void terminate() {
-
+	public void terminate(String containerId) {
+		dockerContainerClient.getDockerClient()
+			.stopContainerCmd(containerId)
+			.exec();
 	}
 
-	@Override
-	public void pull() {
-		try {
-			pull(IMAGE_ALPINE, "latest");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void pull(String image, String tag) throws InterruptedException {
-		dockerContainerClient.getDockerClient().pullImageCmd(image)
-			.withTag(tag)
+	public void pull(DockerImages image) throws InterruptedException {
+		dockerContainerClient.getDockerClient()
+			.pullImageCmd(image.getRepository())
+			.withTag(image.getTag())
 			.start()
 			.awaitCompletion();
 	}
