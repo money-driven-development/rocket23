13a69d82b432ccd71e89439308678b59049de769
Floodnut <gsniper777@gmail.com>
2023-06-10 00:20:26 +0900

    [IC-18] feat: 도커 이미지 풀링 중 발생하는 오류를 추가 (#9)

--- docker/src/main/java/com/initcloud/dockerapi/common/enums/ResponseCode.java
+++ docker/src/main/java/com/initcloud/dockerapi/common/enums/ResponseCode.java
@@ -37,9 +37,10 @@ public enum ResponseCode {
 
 	/**
 	 * 53xx: Container Exception
-	 * @Todo - 추후, 컨테이너 관련 예외는 4xx와 5xx 에러를 명확하게 구분해야 함.
+	 * Todo - 추후, 컨테이너 관련 예외는 4xx와 5xx 에러를 명확하게 구분해야 함.
 	 */
-	NULL_DOCKER_CLIENT(4305, HttpStatus.INTERNAL_SERVER_ERROR, "실행 중인 도커 데몬 정보가 없습니다.");
+	NULL_DOCKER_CLIENT(5305, HttpStatus.INTERNAL_SERVER_ERROR, "실행 중인 도커 데몬 정보가 없습니다."),
+	DOCKER_CANNOT_PULL_IMAGE(5306, HttpStatus.INTERNAL_SERVER_ERROR, "이미지 풀링 중 오류가 발생했습니다.");
 
 	private final int code;
 	private final HttpStatus httpStatus;
