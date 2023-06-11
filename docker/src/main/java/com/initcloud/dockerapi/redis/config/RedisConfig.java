35cda294a7e711b5caa0b22e73adf90b1b748030
Floodnut <gsniper777@gmail.com>
2023-06-11 14:02:51 +0900

    [IC-18] feat: 컨테이너 관리를 위한 빈 클래스 추가  (#9)

--- docker/src/main/java/com/initcloud/dockerapi/redis/config/RedisConfig.java
+++ docker/src/main/java/com/initcloud/dockerapi/redis/config/RedisConfig.java
@@ -1,40 +1,36 @@
 package com.initcloud.dockerapi.redis.config;
 
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
+	@Value("${spring.redis.host}")
+	private String host;
+
+	@Value("${spring.redis.port}")
+	private int port;
+
+	@Value("${spring.redis.topic}")
+	private String topic;
 
 	private final RedisTemplate<String, String> redisTemplate;
 
 	@Bean
-	MessageListenerAdapter messageListener() {
-		return new MessageListenerAdapter(new RedisMessageSubscriber());
-	}
-
-	@Bean
-	RedisMessagePublisher redisPublisher() {
-		return new RedisMessagePublisher(redisTemplate, topic());
-	}
-
-	@Bean
 	ChannelTopic topic() {
-		return new ChannelTopic("default");
+		return new ChannelTopic(topic);
 	}
 
 	@Bean
-	RedisMessageListenerContainer redisContainer() {
-		final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-		container.setConnectionFactory(redisTemplate.getConnectionFactory());
-		container.addMessageListener(messageListener(), topic());
-		return container;
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new RedissonConnectionFactory();
 	}
 }
