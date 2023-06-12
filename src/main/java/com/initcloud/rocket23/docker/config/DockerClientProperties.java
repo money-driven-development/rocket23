a972143004fe937d9a8fa5e8bc58cd7503e8a249
Floodnut <gsniper777@gmail.com>
2023-06-12 13:44:43 +0900

    [IC-18] fix: 병합 충돌 수정 (#9)

---
+++ src/main/java/com/initcloud/rocket23/docker/config/DockerClientProperties.java
@@ -0,0 +1,8 @@
+package com.initcloud.rocket23.docker.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DockerClientProperties {
+}
