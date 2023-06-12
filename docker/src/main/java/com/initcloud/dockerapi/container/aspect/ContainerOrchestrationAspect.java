8e3b90d81873d37fcd8784df8ac943fe6415c1ce
Floodnut <gsniper777@gmail.com>
2023-06-12 22:44:29 +0900

    [IC-24] feat: 스캔 종료, 컨테이너 종료에 따른 Aspect 로직 추가 (#17)

--- docker/src/main/java/com/initcloud/dockerapi/container/aspect/ContainerOrchestrationAspect.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/aspect/ContainerOrchestrationAspect.java
@@ -4,20 +4,40 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.initcloud.dockerapi.container.client.DockerContainerApi;
+import com.initcloud.dockerapi.redis.client.RedisQueueClient;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class ContainerOrchestrationAspect {
+
+	private final DockerContainerApi dockerContainerApi;
+	private RedisQueueClient redisQueueClient = RedisQueueClient.getRedisQueueClient();
 
 	/**
 	 * 컨테이너 동작 후 스탠바이 컨테이너 수 조정
 	 */
 	@AfterReturning(value = "execution(* com.initcloud.dockerapi.container.service.DockerService.executeContainer(..)) && args(containerId))")
 	public void afterReturningContainerExecute(String containerId) {
-		log.info("[EXECUTE] {}", containerId);
+		log.info("[SCAN FINISHED] {}", containerId);
 
-		/* Todo - 스탠바이 컨테이너 수를 조정하는 로직 추가 */
+		redisQueueClient.addToQueue(containerId);
+	}
+
+	/**
+	 * 컨테이너 종료 후 스탠바이 컨테이너 수 조정
+	 */
+	@AfterReturning(value = "execution(* com.initcloud.dockerapi.container.service.DockerService.terminateContainer(..)) && args(containerId))")
+	public void afterReturningContainerTerminate(String containerId) {
+		log.info("[CONTAINER TERMINATED] {}", containerId);
+
+		CreateContainerResponse newContainer = dockerContainerApi.create();
+		redisQueueClient.addToQueue(newContainer.getId());
 	}
 }
