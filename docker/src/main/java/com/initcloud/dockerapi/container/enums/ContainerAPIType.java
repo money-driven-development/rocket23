6b7c85bc4d0f899334acc2b8a2c6d979177b5407
Floodnut <gsniper777@gmail.com>
2023-06-13 15:49:01 +0900

    [IC-24] feat: 열거형에 주석을 추가 (#17)

--- docker/src/main/java/com/initcloud/dockerapi/container/enums/ContainerAPIType.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/enums/ContainerAPIType.java
@@ -1,5 +1,18 @@
 package com.initcloud.dockerapi.container.enums;
 
 public enum ContainerAPIType {
-	DOCKER, GRPC_CONTAINERD, KUBERNETES
+	/**
+	 * 오케스트레이션 여부와 상관 없이 도커 API 를 사용할 경우
+	 */
+	DOCKER,
+
+	/**
+	 * 오케스트레이션 여부와 상관 없이 Containerd API 를 사용할 경우
+	 */
+	GRPC_CONTAINERD,
+
+	/**
+	 * 쿠버네티스 API를 사용할 경우
+	 */
+	KUBERNETES
 }
--- docker/src/main/java/com/initcloud/dockerapi/container/enums/ContainerStatus.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/enums/ContainerStatus.java
@@ -1,5 +1,14 @@
 package com.initcloud.dockerapi.container.enums;
 
+/**
+ * Todo - 현재 상태 값이 도커 컨테이너에서 제공하는 상태와 상이해 추후 조정 필요.
+ */
 public enum ContainerStatus {
-	STANDBY, WARMUP, RUNNING, STOPPED, ORPHAN, TERMINATED
+
+	STANDBY, // 사용 대기
+	WARMUP,  // 스탠바이 대기
+	RUNNING, // 사용 중
+	STOPPED, // 정지
+	ORPHAN,  // 관리 이탈
+	TERMINATED // 종료됨
 }
