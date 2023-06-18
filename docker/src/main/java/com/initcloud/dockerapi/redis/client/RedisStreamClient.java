77c86081fb39b0d74c7e7c0b5986d162397f455d
Floodnut <gsniper777@gmail.com>
2023-06-18 16:33:12 +0900

    [IC-27] feat: 레디스 스트림 클라이언트를 추가 (#19)

---
+++ docker/src/main/java/com/initcloud/dockerapi/redis/client/RedisStreamClient.java
@@ -0,0 +1,43 @@
+package com.initcloud.dockerapi.redis.client;
+
+import java.util.Map;
+
+import org.redisson.Redisson;
+import org.redisson.api.RStream;
+import org.redisson.api.RedissonClient;
+
+import org.redisson.client.codec.StringCodec;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.initcloud.dockerapi.redis.message.StreamMessage;
+
+import lombok.Getter;
+
+public class RedisStreamClient implements InitializingBean {
+
+	private static final String STREAM_KEY = "container.docker";
+	private static final RedisStreamClient redisStreamClient = new RedisStreamClient();
+
+	@Getter
+	private RedissonClient redissonClient;
+
+	@Getter
+	private RStream<String, StreamMessage> rStream;
+
+	public RedisStreamClient() {
+		this.redissonClient = Redisson.create();
+		this.rStream = redissonClient.getStream(STREAM_KEY, StringCodec.INSTANCE);
+	}
+
+	/**
+	 * 싱글톤 패턴을 통해서 RedisStreamClient 에 대한 단일 인스턴스만 제공
+	 */
+	public static RedisStreamClient getRedisStreamClient() {
+		return redisStreamClient;
+	}
+
+	@Override
+	public void afterPropertiesSet() {
+		this.rStream = redissonClient.getStream(STREAM_KEY, StringCodec.INSTANCE);
+	}
+}
