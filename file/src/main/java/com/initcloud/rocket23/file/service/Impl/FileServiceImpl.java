82bf0b77e815cfc7e1012cb0a1a627b215395e72
pj991207 <pj991207@gmail.com>
2023-06-22 01:56:33 +0900

    [IC-33] chore: 주석추가 및 if문 수정 (#21)

--- file/src/main/java/com/initcloud/rocket23/file/service/Impl/FileServiceImpl.java
+++ file/src/main/java/com/initcloud/rocket23/file/service/Impl/FileServiceImpl.java
@@ -33,7 +33,12 @@ public class FileServiceImpl implements FileService {
 	@Value("${spring.servlet.multipart.location}")
 	private String uploadPath;
 
-	//path입력을 통해서 uuid 디렉토리를 생성함.
+	/**
+	 *
+	 * @param path 파일 저장 root 경로
+	 * 파일 저장 root 경로 디렉토리 생성
+	 *
+	 */
 	@Override
 	public void init(Path path) {
 		try {
@@ -43,22 +48,27 @@ public class FileServiceImpl implements FileService {
 		}
 	}
 
+	/**
+	 *
+	 * @param file 업로드된 파일
+	 * uuid값을 통해 파일 별도의 디렉토리 이름 생성 -> 파일 저장 root 위치
+	 *
+	 */
 	@Override
 	public void store(MultipartFile file) {
 		try {
 			if (file.isEmpty()) {
 				throw new ApiException(ResponseCode.DATA_MISSING);
 			}
-			//기존의 파일 저장위치와 uuid 값을 결합하여, uuid별 디렉토리를 생성
 			String uuid = UUID.randomUUID().toString();
 			String url = uploadPath + uuid;
 			Path root = Paths.get(url);
-			boolean zipCheck = false;
+			boolean isZipFile = false;
 			if (!Files.exists(root)) {
 				init(root);
 			}
-			zipCheck = isZip(file);
-			storeFile(file, root, zipCheck);
+			isZipFile = isZip(file);
+			storeFile(file, root, isZipFile);
 		} catch (IOException e) {
 			throw new ApiException(ResponseCode.FILE_WRONG_ERROR);
 		} catch (IllegalArgumentException e) {
@@ -68,15 +78,25 @@ public class FileServiceImpl implements FileService {
 		}
 	}
 
+	/**
+	 *
+	 * @param file 업로드된 파일
+	 * @param path 파일 저장 root 경로
+	 * @param isZipFile zip파일인지 확인
+	 * @throws IOException file.getinputstream의 경우 IOException이 발생
+	 * 파일에 대해 저장, zip파일 유무에 따라 unzip호출
+	 * todo file을 db에 저장할 떄 저장위치에 관해서 수정해야함.
+	 *
+	 */
 	@Override
-	public void storeFile(MultipartFile file, Path path, boolean check) throws IOException {
-		if (!check) {
+	public void storeFile(MultipartFile file, Path path, boolean isZipFile) throws IOException {
+		if (isZipFile) {
+			unZip(file, path);
+		} else {
 			try (InputStream inputStream = file.getInputStream()) {
 				Files.copy(inputStream, path.resolve(file.getOriginalFilename()),
 					StandardCopyOption.REPLACE_EXISTING);
 			}
-		} else {
-			unZip(file, path);
 		}
 		save(file, "local", path.toString());
 	}
@@ -109,7 +129,7 @@ public class FileServiceImpl implements FileService {
 	/**
 	 *
 	 * @param file 업로드된 파일
-	 * @param path 저장할 root 경로
+	 * @param path 파일 저장 root 경로
 	 * @throws IOException file.getinputstream의 경우 IOException이 발생
 	 * @throws IllegalArgumentException zipinputstream의 경우 encoding에서 해당 exception이 발생
 	 *
@@ -127,10 +147,8 @@ public class FileServiceImpl implements FileService {
 				if (zipEntry.getName().endsWith(File.separator)) {
 					Files.createDirectories(newPath);
 				} else {
-					if (newPath.getParent() != null) {
-						if (Files.notExists(newPath.getParent())) {
-							Files.createDirectories(newPath.getParent());
-						}
+					if (newPath.getParent() != null && Files.notExists(newPath.getParent())) {
+						Files.createDirectories(newPath.getParent());
 					}
 					Files.copy(zipInputStream, newPath, StandardCopyOption.REPLACE_EXISTING);
 				}
@@ -143,7 +161,7 @@ public class FileServiceImpl implements FileService {
 	/**
 	 *
 	 * @param zipEntry 저장하고자 하는 zip파일의 요소
-	 * @param path 저장하고자하는 zip파일의 root 경로
+	 * @param path 파일 저장 root 경로
 	 * @return zipentry의 저장 경로
 	 *
 	 * zip 내부 파일에 대한 경로 반환,
