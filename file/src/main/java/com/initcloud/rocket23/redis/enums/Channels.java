174c838a4779f17ebb32448dc74b6c189863e734
pj991207 <pj991207@gmail.com>
2023-06-29 13:42:52 +0900

    [IC-51] feat: Redis configuration 작성 (#27)

---
+++ file/src/main/java/com/initcloud/rocket23/redis/enums/Channels.java
@@ -0,0 +1,19 @@
+package com.initcloud.rocket23.redis.enums;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Channels {
+
+	CONTAINER("channel.container"),
+	SCAN("channel.scan");
+
+	@Getter
+	private String channel;
+
+	Channels(String channel) {
+		this.channel = channel;
+	}
+}
