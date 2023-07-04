8e5ee72b24a0099eb2eff2593c5c60d88f5719d9
pj991207 <pj991207@gmail.com>
2023-07-05 02:31:27 +0900

    [IC-53] chore: scanhistory test 코드 (#30)

---
+++ scanhistory/src/test/java/com/initcloud/scanhistory/ScanhistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initcloud.scanhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanhistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
