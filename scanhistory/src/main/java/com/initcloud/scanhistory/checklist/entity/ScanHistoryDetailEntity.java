20829307ba13a7c52701453bb887f383c465a362
pj991207 <pj991207@gmail.com>
2023-07-08 23:11:28 +0900

    [IC-53] feat: Scan History Detail Entity 개발 (#30)

---
+++ scanhistory/src/main/java/com/initcloud/scanhistory/checklist/entity/ScanHistoryDetailEntity.java
@@ -0,0 +1,57 @@
+package com.initcloud.scanhistory.checklist.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "SCAN_HISTORY_DETAIL")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ScanHistoryDetailEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "SCANHISTORYDETAIL_ID")
+	private Long id;
+
+	@ManyToOne 	
+	@JoinColumn(name = "HiSTORY_ID")
+	private ScanHistoryEntity scanHistoryEntity;
+
+	@Column(name = "CHECk_TYPE")
+	@NotNull
+	private String checkType;
+
+	@Column(name = "TARGET_FILE_NAME")
+	@NotNull
+	private String targetFileName;
+
+	@Column(name = "APP_TYPE")
+	@NotNull
+	private String appType;
+
+	@Column(name = "PROVIDER")
+	@NotNull
+	private String provider;
+
+	@Column(name = "SCAN_RESULT")
+	@NotNull
+	private String scanResult;
+
+	@Column(name = "LINE")
+	@NotNull
+	private String line;
+
+	@Column(name = "CODE")
+	@NotNull
+	private String code;
+}
