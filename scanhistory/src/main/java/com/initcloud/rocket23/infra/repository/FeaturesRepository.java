58ab3e3370a2810fef2dad48027d0cad9f99f770
Kuemjong <gsniper777@gmail.com>
2023-07-13 23:47:36 +0900

    [IC-54] feat: 기본 스켈레톤 서비스, 엔터티 및 레포지토리 설정 (#32)

---
+++ scanhistory/src/main/java/com/initcloud/rocket23/infra/repository/FeaturesRepository.java
@@ -0,0 +1,8 @@
+package com.initcloud.rocket23.infra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.initcloud.rocket23.role.entity.Features;
+
+public interface FeaturesRepository extends JpaRepository<Features, Long> {
+}
