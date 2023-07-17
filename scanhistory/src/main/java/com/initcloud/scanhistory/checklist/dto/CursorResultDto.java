abf91a13dabbebbfd33c892e09bda96a2e790f5a
pj991207 <pj991207@gmail.com>
2023-07-17 19:30:59 +0900

    [IC-53] feat: dto @getter 추가 (#30)

--- scanhistory/src/main/java/com/initcloud/scanhistory/checklist/dto/CursorResultDto.java
+++ scanhistory/src/main/java/com/initcloud/scanhistory/checklist/dto/CursorResultDto.java
@@ -2,9 +2,12 @@ package com.initcloud.scanhistory.checklist.dto;
 
 import java.util.List;
 
+import lombok.Getter;
+
+@Getter
 public class CursorResultDto {
 	private List<ScanHistoryDto> values;
-	private boolean hasNext;
+	private Boolean hasNext;
 
 	public CursorResultDto(List<ScanHistoryDto> values, Boolean hasNext) {
 		this.values = values;
