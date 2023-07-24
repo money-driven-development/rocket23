418596806277406adb18a6bfb6852e7989d5994b
pj991207 <pj991207@gmail.com>
2023-07-25 00:00:46 +0900

    [IC-53] fix: CursorResultDto 수정 (#30)

--- scanhistory/src/main/java/com/initcloud/scanhistory/checklist/dto/CursorResultDto.java
+++ scanhistory/src/main/java/com/initcloud/scanhistory/checklist/dto/CursorResultDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class CursorResultDto {
-	private List<ScanHistoryDto> values;
+	private List<ScanHistoryDto> values = new ArrayList<>();
 	private Boolean hasNext;
 
 	public CursorResultDto(List<ScanHistoryDto> values, Boolean hasNext) {
