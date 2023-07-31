9239ecd9a3cb79408571c44f0b4b0e590e45f26d
잉퓨 <dayeon620@kakao.com>
2023-07-31 10:07:21 +0900

    [IC-61] chore: 변수 명 Camel case 통일 (#37)

--- file/src/main/java/com/initcloud/rocket23/github/dto/GithubDto.java
+++ file/src/main/java/com/initcloud/rocket23/github/dto/GithubDto.java
@@ -23,47 +23,46 @@ public class GithubDto {
 		private Boolean pull;
 	}
 
-	// @Todo - modify if camel case could applied.
 	@Getter
 	@NoArgsConstructor(access = AccessLevel.PROTECTED)
 	public static class RepositoryInfo {
 		private Long id;
-		private String node_id;
-		private String full_name;
+		private String nodeId;
+		private String fullName;
 		private String visibility;
-		private String created_at;
-		private String updated_at;
+		private String createdAt;
+		private String updatedAt;
 		private String url;
-		private String downloads_url;
-		private String default_branch;
+		private String downloadsUrl;
+		private String defaultBranch;
 		private Permission permissions;
 
 		@Builder
-		public RepositoryInfo(Long id, String node_id, String full_name, String visibility, String created_at,
-			String updated_at, String url, String downloads_url, String default_branch, Permission permissions) {
+		public RepositoryInfo(Long id, String nodeId, String fullName, String visibility, String createdAt,
+			String updatedAt, String url, String downloadsUrl, String defaultBranch, Permission permissions) {
 			this.id = id;
-			this.node_id = node_id;
-			this.full_name = full_name;
+			this.nodeId = nodeId;
+			this.fullName = fullName;
 			this.visibility = visibility;
-			this.created_at = created_at;
-			this.updated_at = updated_at;
+			this.createdAt = createdAt;
+			this.updatedAt = updatedAt;
 			this.url = url;
-			this.downloads_url = downloads_url;
-			this.default_branch = default_branch;
+			this.downloadsUrl = downloadsUrl;
+			this.defaultBranch = defaultBranch;
 			this.permissions = permissions;
 		}
 
 		public static RepositoryInfo toApiRepository(final RepositoryInfo repo) {
 			return RepositoryInfo.builder()
 				.id(repo.getId())
-				.node_id(repo.getNode_id())
-				.full_name(repo.getFull_name())
+				.nodeId(repo.getNodeId())
+				.fullName(repo.getFullName())
 				.visibility(repo.getVisibility())
-				.created_at(repo.getCreated_at())
-				.updated_at(repo.getUpdated_at())
+				.createdAt(repo.getCreatedAt())
+				.updatedAt(repo.getUpdatedAt())
 				.url(repo.getUrl().replace(GITHUB_API_BASE_URL, ""))
-				.downloads_url(repo.getDownloads_url().replace(GITHUB_API_BASE_URL, ""))
-				.default_branch(repo.getDefault_branch())
+				.downloadsUrl(repo.getDownloadsUrl().replace(GITHUB_API_BASE_URL, ""))
+				.defaultBranch(repo.getDefaultBranch())
 				.permissions(repo.getPermissions())
 				.build();
 		}
