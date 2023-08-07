ba892c6fa1e5091712c877c370ed6282dbe612b7
pj991207 <pj991207@gmail.com>
2023-08-07 21:52:26 +0900

    [IC-71] feat: 단일 스캔 이력(성공,실패,스킵) 내역 개발 (controller,repository,service,dto)

--- scanhistory/src/main/java/com/initcloud/rocket23/checklist/service/ScanHistoryService.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/checklist/service/ScanHistoryService.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Pageable;
 
 import com.initcloud.rocket23.checklist.dto.CursorResultDto;
 import com.initcloud.rocket23.checklist.dto.HistoryDto;
+import com.initcloud.rocket23.checklist.dto.ScanResultDto;
 
 public interface ScanHistoryService {
-	List<HistoryDto> getHistoryList(Long teamId, Long projectId);
-
-	Page<HistoryDto> getOffsetPageHistoryList(Long teamId, Long projectId, Pageable page);
-
-	CursorResultDto getCursorPageHistoryList(Long teamId, Long cursorId, Pageable Page);
+	ScanResultDto getScanHistory(Long teamId, Long projectId, String fileHash);
+	// List<HistoryDto> getHistoryList(Long teamId, Long projectId);
+	//
+	// Page<HistoryDto> getOffsetPageHistoryList(Long teamId, Long projectId, Pageable page);
+	//
+	// CursorResultDto getCursorPageHistoryList(Long teamId, Long cursorId, Pageable Page);
 }
