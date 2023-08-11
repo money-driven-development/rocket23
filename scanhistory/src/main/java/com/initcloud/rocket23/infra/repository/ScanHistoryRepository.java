db4c4ca6c4c2a92a730f4dcbaba5e772ad3d0bad
pj991207 <pj991207@gmail.com>
2023-08-11 20:13:59 +0900

    [IC-76] fix: 단일 스캔 조회 Repository 누락 내역 추가 (#45)

--- scanhistory/src/main/java/com/initcloud/rocket23/infra/repository/ScanHistoryRepository.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/infra/repository/ScanHistoryRepository.java
@@ -1,5 +1,7 @@
 package com.initcloud.rocket23.infra.repository;
 
+import java.util.Optional;
+
 import com.initcloud.rocket23.checklist.entity.ScanHistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScanHistoryRepository extends JpaRepository<ScanHistory, Long> {
-
+	Optional<ScanHistory> findTopByTeamIdAndProjectIdAndFileHashOrderById(Long teamId, Long projectId, String hashCode);
 }
