5ad1b6796abec1d60f9ad8bd7afe2da155d38e76
Kuemjong <gsniper777@gmail.com>
2023-08-18 23:55:32 +0900

    [IC-84] feat: 스프링 시큐리티 swagger uri 설정 (#53)

--- scanhistory/src/main/java/com/initcloud/rocket23/ScanhistoryApplication.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/ScanhistoryApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableWebMvc
 @EnableOpenApi
 @EnableFeignClients(basePackages = {"com.initcloud.rocket23.common.feign"})
 @EnableJpaAuditing
