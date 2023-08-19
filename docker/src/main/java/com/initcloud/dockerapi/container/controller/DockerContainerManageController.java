685e65d9c57e4a4924bca89ada45021360f874a6
Kuemjong <gsniper777@gmail.com>
2023-08-19 15:21:36 +0900

    [IC-84] feat: 스캔 API 스웨거 설정 (#53)

--- docker/src/main/java/com/initcloud/dockerapi/container/controller/DockerContainerManageController.java
+++ docker/src/main/java/com/initcloud/dockerapi/container/controller/DockerContainerManageController.java
@@ -1,5 +1,8 @@
 package com.initcloud.dockerapi.container.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +25,14 @@ public class DockerContainerManageController {
 
 	private final DockerManageService dockerService;
 
+	@ApiOperation(value = "Scan IaC", notes = "Scan IaC", response = ResponseDto.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "Authorization", paramType = "header", value = "Access Token", required = true, dataTypeClass = String.class),
+			@ApiImplicitParam(name = "request", paramType = "body", value = "Scan request", required = true, dataTypeClass = IaCScanRequestDto.class)})
 	@PostMapping
-	public ResponseDto containerRun(@RequestBody IaCScanRequestDto path) {
+	public ResponseDto containerRun(@RequestBody IaCScanRequestDto request) {
 
-		ContainerDto response = dockerService.executeContainer(1, path);
+		ContainerDto response = dockerService.executeContainer(1, request);
 
 		return new ResponseDto<>(response);
 	}
