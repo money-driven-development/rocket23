042d7cf3ec41a2f3f68cbcadeac5ec29f2f89220
Kuemjong <gsniper777@gmail.com>
2023-09-07 00:31:57 +0900

    [IC-88] fix: 스프링 시큐리티 필터 추가 (#57)

--- scanhistory/src/main/java/com/initcloud/rocket23/security/config/SecurityConfig.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.initcloud.rocket23.security.config;
 
 import com.initcloud.rocket23.security.entrypoint.TokenGlobalEntryPoint;
+import com.initcloud.rocket23.security.filter.TokenAuthenticationFilter;
 import com.initcloud.rocket23.security.provider.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,7 +38,12 @@ public class SecurityConfig {
                 .antMatchers("/auth/**", "/demo/**", "/rocket/**").permitAll()
                 .and()
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(tokenGlobalEntryPoint)
+                .and()
+                .addFilterBefore(new TokenAuthenticationFilter(jwtProvider, securityProperties),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
