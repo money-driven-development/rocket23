fdcdc52b8d89bd11a540e0083d6c586a6e398d89
Kuemjong <gsniper777@gmail.com>
2023-09-11 23:13:17 +0900

    [IC-98] feat: 레디스 구성 정보 변경 (#68)

--- docker/src/main/java/com/initcloud/dockerapi/redis/config/RedisClientConfig.java
+++ docker/src/main/java/com/initcloud/dockerapi/redis/config/RedisClientConfig.java
@@ -4,17 +4,30 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisClientConfig {
-	@Bean
-	public RedissonClient redissonClient() {
-		Config config = new Config();
-		config.setCodec(new JsonJacksonCodec())
-			.useSingleServer()
-			.setAddress("redis://127.0.0.1:6379"); // Todo - 환경 변수로 옮길 예정
-		return Redisson.create(config);
-	}
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.setCodec(new JsonJacksonCodec())
+                .useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + Integer.toString(redisPort))
+                .setPassword(redisPassword);
+
+        return Redisson.create(config);
+    }
 }
