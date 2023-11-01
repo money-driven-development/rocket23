d691ed51d1bb696354476bf19d054501496a4191
잉퓨 <dayeon620@kakao.com>
2023-11-01 17:15:28 +0900

    [IC-108] feat: github request null 관련 예외처리 진행 (#89)

--- scanhistory/src/main/java/com/initcloud/rocket23/security/service/OAuthService.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/security/service/OAuthService.java
@@ -9,13 +9,12 @@ import com.initcloud.rocket23.security.dto.Token;
 import com.initcloud.rocket23.security.facade.OAuthRequestFacade;
 import com.initcloud.rocket23.user.dto.AuthRequestDto;
 import com.initcloud.rocket23.user.entity.User;
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -36,13 +35,21 @@ public class OAuthService {
     }
 
     public OAuthDto.GithubTokenResponse getAccessToken(AuthRequestDto request) {
+        if (request == null
+                || request.getClientId() == null
+                || request.getClientSecret() == null
+                || request.getCode() == null) {
+            throw new ApiAuthException(ResponseCode.INVALID_REQUEST);
+        }
+
         return oauthRequestFacade.requestGithubOAuthToken(request);
     }
 
     public Token getUserAccessToken(String clientId, String clientSecret, String code, String redirect) {
         AuthRequestDto request = new AuthRequestDto(clientId, clientSecret, code, redirect);
         OAuthDto.GithubTokenResponse tokenResponse = oauthRequestFacade.requestGithubOAuthToken(request);
-        OAuthDto.GithubUserDetail userDetail = oauthRequestFacade.requestGithubUserDetail(tokenResponse.getAccessToken());
+        OAuthDto.GithubUserDetail userDetail = oauthRequestFacade.requestGithubUserDetail(
+                tokenResponse.getAccessToken());
 
         User user = getUserIfExist(userDetail);
 
@@ -63,8 +70,9 @@ public class OAuthService {
     public User getUserIfExist(OAuthDto.GithubUserDetail userDetail) {
         Optional<User> user = userRepository.findUserByUsername(userDetail.getLogin());
 
-        if (user.isPresent())
+        if (user.isPresent()) {
             return user.get();
+        }
 
         User socialUser = userRepository.save(User.addIndividualSocialUser(userDetail));
 
