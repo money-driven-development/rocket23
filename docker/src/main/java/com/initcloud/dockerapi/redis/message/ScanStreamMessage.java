c475d92ed28d0dcdb3d40e13d7f30a1d6b564cf5
inpyu <dayeon620@naver.com>
2024-01-11 15:57:36 +0900

    [IC-119] feat: docker api 내 file Hash 전달 (#96)

--- docker/src/main/java/com/initcloud/dockerapi/redis/message/ScanStreamMessage.java
+++ docker/src/main/java/com/initcloud/dockerapi/redis/message/ScanStreamMessage.java
@@ -59,13 +59,14 @@ public class ScanStreamMessage<T> implements Serializable {
                 .build();
     }
 
-    public static String serializeToScanResult(String rawData, String teamCode, String projectCode) throws ParseException {
+    public static String serializeToScanResult(String rawData, String teamCode, String projectCode, String fileHash) throws ParseException {
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = (JSONObject) parser.parse(rawData);
 
         // 팀, 프로젝트 정보 추가
         jsonObject.put("teamCode", teamCode);
         jsonObject.put("projectCode", projectCode);
+        jsonObject.put("fileHash", fileHash);
 
         // 불필요한 필드 삭제 로직 추가 가능
 
