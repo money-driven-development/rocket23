d4cab4b9f2aca443c7610b5fd177f373e0abbcbc
inpyu <dayeon620@naver.com>
2024-01-17 16:46:39 +0900

    [IC-120] feat: tag 엔티티 개발 (#99)

---
+++ scanhistory/src/main/java/com/initcloud/rocket23/checklist/entity/scanHistory/Tag.java
@@ -0,0 +1,41 @@
+package com.initcloud.rocket23.checklist.entity.scanHistory;
+
+import com.initcloud.rocket23.policy.entity.BasePolicy;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "TAG")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    @JoinColumn(name = "policy_id")
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private BasePolicy basePolicy;
+
+    @Column(name = "default_policy_name_ic")
+    private String defaultPolicyNameIc;
+
+    @Column(name = "tag")
+    @Enumerated(EnumType.STRING)
+    private com.initcloud.rocket23.common.enums.Tag tag;
+
+}
