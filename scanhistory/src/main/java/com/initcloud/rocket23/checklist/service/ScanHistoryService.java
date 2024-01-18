44393464d5abe85aa0eebdbd7fa2c77f946933a3
inpyu <dayeon620@naver.com>
2024-01-18 11:05:03 +0900

    [IC-110] chore: 스캔 응답 형식 변경 (#92)

--- scanhistory/src/main/java/com/initcloud/rocket23/checklist/service/ScanHistoryService.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/checklist/service/ScanHistoryService.java
@@ -16,7 +16,7 @@ public interface ScanHistoryService {
 
     List<ScanResultDto.Summary> getScanHistoryAll(String teamCode, String projectCode);
 
-    String getScanSuccess(String fileHash);
+    boolean getScanSuccess(String fileHash);
     // List<HistoryDto> getHistoryList(String teamCode, String projectCode,;
     //
     // Page<HistoryDto> getOffsetPageHistoryList(String teamCode, String projectCode, Pageable page);
