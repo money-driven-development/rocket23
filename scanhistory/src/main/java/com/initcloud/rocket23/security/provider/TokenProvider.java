db625ca20de20f85f3ccf73974b86a5e76474367
잉퓨 <dayeon620@kakao.com>
2024-02-27 15:59:22 +0900

    [IC-127] chore: 명시적 표현 변경 (#110)

--- scanhistory/src/main/java/com/initcloud/rocket23/security/provider/TokenProvider.java
+++ scanhistory/src/main/java/com/initcloud/rocket23/security/provider/TokenProvider.java
@@ -2,8 +2,11 @@ package com.initcloud.rocket23.security.provider;
 
 public interface TokenProvider {
 
-	long EXPIRED_TIME_ACCESS = 30 * 60 * 1000; // 30 minutes for access token
-	long EXPIRED_TIME_REFRESH = 7 * 24 * 60 * 60 * 1000; // 7 days for refresh token
+	long MINUTE = 6 * 30;
+	long DAY = 24 * 60 * MINUTE ;
+
+	long EXPIRED_TIME_ACCESS = 30 * MINUTE; // 30 minutes for access token
+	long EXPIRED_TIME_REFRESH = 7 * DAY; // 7 days for refresh token
 
 	boolean validate(String token, String key);
 }
